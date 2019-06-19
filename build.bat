d604ce53ab7fc15940b2766fb0d976e78823e762
NicEastvillage <nicoesterby@gmail.com>
2019-06-19 16:04:17 +0200

    First commit.

---
+++ build.bat
@@ -0,0 +1,4 @@
+IF NOT EXIST RLUtilities (
+git clone https://github.com/samuelpmish/RLUtilities.git
+)
+cmake -A x64 -B build
---
+++ src/examplebot.cc
@@ -0,0 +1,10 @@
+#include "rlbot/bot.h"
+
+Bot::Bot(int _index, int _team, std::string _name)
+{
+}
+
+Input Bot::GetOutput(Game game)
+{
+	return Input();
+}
